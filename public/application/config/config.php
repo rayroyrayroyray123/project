536fd0704b5da311cff60d4c27ef432f8b9a2f14
rayroyrayroyray123 <54895802+rayroyrayroyray123@users.noreply.github.com>
2022-10-19 15:23:23 +1000

    a

--- public/application/config/config.php
+++ public/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8181/';
+$config['base_url'] = 'http://34.136.42.53:8181/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost:8181/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = ' ';
 
 /*
 |--------------------------------------------------------------------------
